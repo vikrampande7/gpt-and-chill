5f18348a29570ec656f142cea53f4f0d9620d9f7
Vikram Pande <vikrampande783@gmail.com>
2024-02-20 16:00:32 -0500

    add pytorch basics

---
+++ 5_pytorch_basics.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn
+from torchtyping import TensorType
+
+# Helpful functions:
+# https://pytorch.org/docs/stable/generated/torch.reshape.html
+# https://pytorch.org/docs/stable/generated/torch.mean.html
+# https://pytorch.org/docs/stable/generated/torch.cat.html
+# https://pytorch.org/docs/stable/generated/torch.nn.functional.mse_loss.html
+
+# Round your answers to 4 decimal places using torch.round(input_tensor, decimals = 4)
+class Solution:
+    def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
+        # torch.reshape() will be useful - check out the documentation
+        m, n = to_reshape.shape
+        return torch.reshape(to_reshape, (m * n // 2, 2))
+
+    def average(self, to_avg: TensorType[float]) -> TensorType[float]:
+        # torch.mean() will be useful - check out the documentation
+        return torch.mean(to_avg, dim=0)
+
+    def concatenate(self, cat_one: TensorType[float], cat_two: TensorType[float]) -> TensorType[float]:
+        # torch.cat() will be useful - check out the documentation
+        return torch.cat((cat_one, cat_two),1)
+
+    def get_loss(self, prediction: TensorType[float], target: TensorType[float]) -> TensorType[float]:
+        # torch.nn.functional.mse_loss() will be useful - check out the documentation
+        return torch.nn.functional.mse_loss(prediction, target)
